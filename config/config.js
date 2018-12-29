046e24240ba9f50aaa6bf8c1cb7b525a76271329
Lex51138 <q372264507@gmail.com>
2018-12-29 10:26:13 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     port:process.env.PORT ||(process.env.NODE_ENV === 'production'?8080:7777),
     apiHost:process.env.APIHOST || 'localhost',
     apiPort:process.env.APIPORT || '8791',
-    dbHost:"47.107.187.0",
-    dbPort:"37017",
+    dbHost:"localhost",
+    dbPort:"27017",
     app:{
         title:"personal blog",
         description:'Nealyang\'s personal blog demo',
