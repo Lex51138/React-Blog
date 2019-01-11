e2acdfc898c667c8f6b56762f3d0ca24d8468da3
lex51138 <q372264507@gmail.com>
2019-01-12 00:16:04 +0800

    no message

--- config/config.js
+++ config/config.js
@@ -1,10 +1,8 @@
 module.exports = {
     host:process.env.HOST || 'localhost',
-    port:process.env.PORT ||(process.env.NODE_ENV === 'production'?8080:7777),
+    port:process.env.PORT ||(process.env.NODE_ENV === 'production'?80:7777),
     apiHost:process.env.APIHOST || 'localhost',
     apiPort:process.env.APIPORT || '8791',
-    dbHost:"localhost",
-    dbPort:"27017",
     app:{
         title:"personal blog",
         description:'Lex\'s personal blog demo',
