11f9ed253d0c08f5425b30dd3d14654e52ce0e90
lex51138 <q372264507@gmail.com>
2019-01-19 06:38:01 +0800

    解决了prod打包样式会有点瑕疵

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,39 +13,35 @@ console.log(pathLib.resolve(ENTRY_PATH, 'index.js'));
 
 module.exports = {
     entry: {
-        index: ['babel-polyfill',pathLib.resolve(ENTRY_PATH, 'index.js')],
-        vendor: ['react', 'react-dom', 'react-router-dom','react-router']
+        index: ['babel-polyfill', pathLib.resolve(ENTRY_PATH, 'index.js')],
+        vendor: ['react', 'react-dom', 'react-router-dom', 'react-router']
     },
     output: {
         path: OUTPUT_PATH,
         publicPath: '/',
         filename: '[name]-[chunkhash].js'
     },
-   // devtool: 'source-map',
+    // devtool: 'source-map',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
             {
-                test:/\.css$/,
-                exclude:/node_modules/,
-                use:ExtractTextPlugin.extract({
-                    fallback:'style-loader',
-                    use:[
-                        {
-                            loader:'css-loader',
-                            options:{
-                                modules:true,
-                                localIdentName:'[name]-[local]-[hash:base64:5]',
-                                importLoaders:1
-                            }
-                        },
-                        'postcss-loader'
-                    ]
-                })
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]-[local]-[hash:base64:5]',
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ]
             },
             {
                 test: /\.css$/,
@@ -64,14 +60,12 @@ module.exports = {
             {
                 test: /\.(png|jpg|gif|JPG|GIF|PNG|BMP|bmp|JPEG|jpeg)$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192
-                        }
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
                     }
-                ]
+                }]
             },
             {
                 test: /\.(eot|woff|ttf|woff2|svg)$/,
@@ -82,33 +76,26 @@ module.exports = {
     plugins: [
         new CleanPlugin(['build']),
         new ProgressBarPlugin(),
-        new webpack.optimize.AggressiveMergingPlugin(),//改善chunk传输
+        new webpack.optimize.AggressiveMergingPlugin(), //改善chunk传输
         new webpack.DefinePlugin({
             "progress.env.NODE_ENV": JSON.stringify('production')
         }),
         new HtmlWebpackPlugin({ //构建html
             title: "Lex blog",
-            inject: true, // 自动注入
-            minify: {
-                removeComments: true,        //去注释
-                collapseWhitespace: true,    //压缩空格
-                removeAttributeQuotes: true  //去除属性引用
-                // more options:
-                // https://github.com/kangax/html-minifier#options-quick-reference
-            },
-            //必须通过上面的 CommonsChunkPlugin 的依赖关系自动添加 js，css 等
-            chunksSortMode: 'dependency',
             showErrors: true,
-            meta:{viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'} //加入meta自适应至手机端
+            meta: {
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+            } //加入meta自适应至手机端
         }),
-        new webpack.NoEmitOnErrorsPlugin(),//保证出错时页面不阻塞，且会在编译结束后报错
+        new webpack.NoEmitOnErrorsPlugin(), //保证出错时页面不阻塞，且会在编译结束后报错
+        new webpack.HashedModuleIdsPlugin(), //用 HashedModuleIdsPlugin 可以轻松地实现 chunkhash 的稳定化
         new ExtractTextPlugin({
-            filename:'bundle.[contenthash].css',
-            disable:false,
-            allChunks:true
+            filename: 'bundle.[contenthash].css',
+            disable: false,
+            allChunks: true
         }),
         new ExtractTextPlugin('[name].[contenthash].css'),
-        new webpack.HashedModuleIdsPlugin(),//用 HashedModuleIdsPlugin 可以轻松地实现 chunkhash 的稳定化
+       
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             minChunks: function (module) {
@@ -117,23 +104,18 @@ module.exports = {
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: "manifest"
-        }), 
-        new webpack.optimize.UglifyJsPlugin({//打包优化
-            comments: false,//去除注释
+        }),
+        new webpack.optimize.UglifyJsPlugin({ //打包优化
+            comments: false, //去除注释
             compress: {
-              warnings: false//去除警告
+                warnings: false //去除警告
             }
-          }),
-          new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-             }
-         }),
-         new CompressionWebpackPlugin({ //gzip 压缩
+        }),
+        new CompressionWebpackPlugin({ //gzip 压缩
             asset: '[path].gz[query]',
             algorithm: 'gzip',
             test: new RegExp(
-                '\\.(js|css|jpg|png)$'    //压缩 js 与 css
+                '\\.(js|css|jpg|png)$' //压缩 js 与 css
             ),
             threshold: 10240,
             minRatio: 0.8
@@ -143,4 +125,3 @@ module.exports = {
         extensions: ['.js', '.json', '.sass', '.scss', '.less', 'jsx']
     },
 };
-
